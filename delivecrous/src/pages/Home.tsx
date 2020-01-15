6f16ec4489db978538a3c556794e760ed24f5c02
AntoineBdns <antoine.boudens@gmail.com>
2020-01-15 15:26:07 +0100

    API CALL

--- delivecrous/src/pages/Home.tsx
+++ delivecrous/src/pages/Home.tsx
@@ -1,18 +1,43 @@
-import {IonList,IonModal,IonToast,IonItem,IonIcon,IonButton,IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonFab, IonFabButton, IonCheckbox, IonInput, IonListHeader } from '@ionic/react';
+import {IonAlert,IonList,IonModal,IonToast,IonItem,IonIcon,IonButton,IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonInput, IonListHeader } from '@ionic/react';
 import React, { useState } from 'react';
 import  {cart,add,close} from 'ionicons/icons';
 import "./Home.css";
 import data from "../data.json"
 const shopList = data.shopList;
+
 const Home: React.FC = () => {
+ 
+  // function getBeers(){
+  //   fetch("https://api.punkapi.com/v2/beers")
+  //   .then(res => res.json())
+  //   .then(
+  //     (result) => {
+  //       console.log("API CALL");
+  //       setBeerList(result);
+  //     },
+  //     (error) => {
+  //       alert(error);
+  //     }
+  //   );
+  // }
   
   const [showModal, setShowModal] = useState(false);
-  const [cartList, setCartList] = useState(new Array());
+  const [cartList, setCartList] = useState(new Array<number>());
   const [showToastAdd, setShowToastAdd] = useState(false);
+  const [showAlertValidation, setAlertValidation] = useState(false);
+  const [showToastValidation, setShowToastValidation] = useState(false);
+  //const [beerList, setBeerList] = useState(new Array());
+  
+  // if(beerList.length <= 0)
+  //   getBeers();
 
   function addItemToCart(id : number){
     cartList.push(id);
     setShowToastAdd(true);
+  }
+
+  function validateForm(){
+    setAlertValidation(true);
   }
 
   //Affiche les cards des shops sur la page principale
@@ -33,11 +58,28 @@ const Home: React.FC = () => {
     )
   });
 
+  // var beerCarts = beerList.map((beer : any)=>{
+  //   return (<IonCard className="card">
+  //     <img src= {beer.image_url} alt={beer.name} />
+  //     <IonCardHeader>
+  //       <IonCardTitle className="title">{beer.name}</IonCardTitle>
+  //       <IonCardSubtitle className="price">{beer.id}</IonCardSubtitle>
+  //     </IonCardHeader>
+  //     <IonCardContent className="content">
+  //       {beer.description}
+  //       <IonButton expand="block" slot="end" onClick={ () => addItemToCart(beer.id)}>
+  //           Ajouter
+  //       </IonButton>
+  //     </IonCardContent>    
+  //   </IonCard>
+  //   )
+  // });
+
   //Affiche la liste des items dans le pannier
   var cartCards = cartList.map((id : number) => {
     var name = null;
     var price = null;
-    shopList.map((s) => {
+    shopList.forEach((s) => {
       if(s.id == id){
          name = s.name;
          price = s.price;
@@ -64,7 +106,11 @@ const Home: React.FC = () => {
       </IonHeader>   
       <IonContent className="ion-padding">
         <IonLabel><h1>La carte</h1></IonLabel>
-        {shopCards}
+        {/*
+        beerCarts
+        */
+        shopCards
+        }
         <IonModal isOpen={showModal}>
           <IonToolbar>
             <IonTitle>Pannier</IonTitle>
@@ -97,7 +143,7 @@ const Home: React.FC = () => {
                   <IonLabel>Code Postal</IonLabel>
                   <IonInput type="number" placeholder="59300"></IonInput>
                 </IonItem>
-                <IonButton expand="block" disabled={cartList.length<=0}>
+                <IonButton expand="block" disabled={cartList.length<=0} onClick={() => validateForm()}>
                     Passer Commande
                 </IonButton>
               </IonCardContent>
@@ -113,6 +159,34 @@ const Home: React.FC = () => {
         position="bottom"
         duration={500}
       />
+      <IonToast
+        isOpen={showToastValidation}
+        onDidDismiss={() => setShowToastValidation(false)}
+        message="Commande envoyée"
+        cssClass="toastMessage"
+        position="bottom"
+        duration={1000}
+      />
+      <IonAlert
+          isOpen={showAlertValidation}
+          onDidDismiss={() => setAlertValidation(false)}
+          header={'Validation de la commande'}
+          message={'Voulez vous vraiement valider cette commande ?'}
+          buttons={[
+            {
+              text: 'Retour',
+              role: 'cancel',
+              cssClass: 'secondary',
+            },
+            {
+              text: 'Valider',
+              handler: () => {
+                setCartList(new Array());
+                setShowToastValidation(true);
+              }
+            }
+          ]}
+        />
     </IonPage>
   );
 };
