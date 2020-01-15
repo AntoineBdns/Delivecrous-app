ef9d4c488a67e55e9109da8db01ab7c373f3d0d2
AntoineBdns <antoine.boudens@gmail.com>
2020-01-15 12:26:23 +0100

    Ajout Pannie + ItemList

--- delivecrous/src/pages/Home.tsx
+++ delivecrous/src/pages/Home.tsx
@@ -1,28 +1,59 @@
-import {IonModal,IonIcon,IonButton,IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonFab, IonFabButton, IonCheckbox } from '@ionic/react';
+import {IonModal,IonToast,IonItem,IonIcon,IonButton,IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonFab, IonFabButton, IonCheckbox } from '@ionic/react';
 import React, { useState } from 'react';
 import  {cart,add,close} from 'ionicons/icons';
 import "./Home.css";
 import data from "../data.json"
 const shopList = data.shopList;
 const Home: React.FC = () => {
-  var itemList = [];
   
   const [showModal, setShowModal] = useState(false);
+  const [cartList, setCartList] = useState(new Array());
+  const [showToastAdd, setShowToastAdd] = useState(false);
 
+  function addItemToCart(id : number){
+    cartList.push(id);
+    setShowToastAdd(true);
+  }
+
+  //Affiche les cards des shops sur la page principale
   var shopCards = shopList.map((shop) => {
     return (<IonCard className="card">
-      <img src= {shop.img} />
+      <img src= {shop.img} alt={shop.name} />
       <IonCardHeader>
         <IonCardTitle className="title">{shop.name}</IonCardTitle>
         <IonCardSubtitle className="price">{shop.price}</IonCardSubtitle>
       </IonCardHeader>
       <IonCardContent className="content">
         {shop.desc}
-      </IonCardContent>
+        <IonButton expand="block" slot="end" onClick={ () => addItemToCart(shop.id)}>
+            Ajouter
+        </IonButton>
+      </IonCardContent>    
     </IonCard>
     )
   });
 
+  //Affiche la liste des shops dans le pannier
+  var cartCards = cartList.map((id : number) => {
+    var name = null;
+    var price = null;
+    shopList.map((s) => {
+      if(s.id == id){
+         name = s.name;
+         price = s.price;
+      }
+    });
+    return (
+      <IonCard className="card">
+        <IonCardHeader>
+          <IonCardTitle className="title">{name}</IonCardTitle>
+          <IonCardSubtitle className="price">{price}</IonCardSubtitle>
+        </IonCardHeader>
+      </IonCard>
+      )
+  });
+
+  //Affiche le contenu da la page
   return (
     <IonPage>
       <IonHeader>
@@ -32,32 +63,31 @@ const Home: React.FC = () => {
               <IonIcon slot="icon-only" icon={cart}/>
           </IonButton>
         </IonToolbar>
-      </IonHeader>
-      
+      </IonHeader>   
       <IonContent className="ion-padding">
         <IonLabel><h1>La carte</h1></IonLabel>
         {shopCards}
-        <IonFab vertical="bottom" horizontal="end" slot="fixed">
-          <IonFabButton disabled={itemList.length<=0}>
-            <IonIcon icon={add}/>
-          </IonFabButton>
-        </IonFab>
-
         <IonModal isOpen={showModal}>
           <IonToolbar>
-            <IonTitle>Delivecrous</IonTitle>
+            <IonTitle>Pannier</IonTitle>
             <IonButton slot="end" onClick={() => setShowModal(false)}>
                 <IonIcon slot="icon-only" icon={close}/>
             </IonButton>
           </IonToolbar>
           <IonContent>
-            <p>This is modal content</p>
+            {cartCards}
           </IonContent>      
         </IonModal>
       </IonContent>
+      <IonToast
+        isOpen={showToastAdd}
+        onDidDismiss={() => setShowToastAdd(false)}
+        message="C'est good"
+        cssClass="toastMessage"
+        position="bottom"
+        duration={500}
+      />
     </IonPage>
-
-    
   );
 };
 
