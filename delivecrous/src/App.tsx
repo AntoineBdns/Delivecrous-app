febbfe6fc1f6d634350e4b90f34ecc31c3124370
Slinth <dorian.decroix@hotmail.fr>
2020-01-21 14:45:43 +0100

    Clean unnecessary files

--- delivecrous/src/App.tsx
+++ delivecrous/src/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
-} from '@ionic/react';
-
+import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { home, contact } from 'ionicons/icons';
+
 import Home from './pages/Home';
 import ItemPage from './pages/ItemPage';
-// import Profile from './pages/Profile';
-
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -42,23 +31,9 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route path="/home" component={Home} exact={true} />
-        {/* <Route path="/profile" component={Profile} exact={true} /> */}
         <Route path="/plat/:id" component={ItemPage} />
         <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
       </IonRouterOutlet>
-      {/* <IonTabs>
-       
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="home" href="/home">
-            <IonIcon icon={home} />
-            <IonLabel>Home</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="profile" href="/profile">
-            <IonIcon icon={contact} />
-            <IonLabel>Profile</IonLabel>
-          </IonTabButton>
-        </IonTabBar> 
-      </IonTabs>*/}
     </IonReactRouter>
   </IonApp>
 );
